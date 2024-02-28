796fa6da951b53c09ce705662cdf2e24367f1bd7
AshleAndTNTALEXEY <alex27200602@gmail.com>
2024-02-28 13:08:52 +0300

    Добавьте файлы проекта.

---
+++ cos.cpp
@@ -0,0 +1,10 @@
+﻿#include <math.h>
+#include <stdio.h>
+main() {
+	double x, y;
+	printf("Введите значение x");
+	scanf("%lf", &x);
+	y = cos(x);
+	printf("Результат: y = %lf/n", y);
+	return 0;
+}
